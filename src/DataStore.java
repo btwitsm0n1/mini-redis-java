0ae32f3302b8079595e0927a8288b94e7ef06c01
btwitsm0n1 <272431606+btwitsm0n1@users.noreply.github.com>
2026-08-31 16:44:08 +0530

    Make DataStore thread-safe

--- src/DataStore.java
+++ src/DataStore.java
@@ -1,27 +1,23 @@
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class DataStore {
 
-    private final Map<String, String> data;
-
-    public DataStore() {
-        data = new HashMap<>();
-    }
+    private final Map<String, String> store = new ConcurrentHashMap<>();
 
     public void set(String key, String value) {
-        data.put(key, value);
+        store.put(key, value);
     }
 
     public String get(String key) {
-        return data.get(key);
+        return store.get(key);
     }
 
     public void delete(String key) {
-        data.remove(key);
+        store.remove(key);
     }
 
     public boolean exists(String key) {
-        return data.containsKey(key);
+        return store.containsKey(key);
     }
 }
